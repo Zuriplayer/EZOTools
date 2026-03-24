66d6732caa96ec990df482deeb61fab8ee4895fb
Zuriplayer <Zuriplayer@gmail.com>
2026-03-24 22:09:39 +0100

    release: prepare v1.0.0

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "3.7.5"
+EZOTools.ADDON_VERSION = "1.0.0"
 EZOTools.AUTHOR        = "@Zuriplayer"
