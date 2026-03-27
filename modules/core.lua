2f142d7f450c32e84d64f234231bcc245b66255a
Zuriplayer <Zuriplayer@gmail.com>
2026-03-27 21:16:02 +0100

    Revert side-slot hover interaction experiment

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.17"
+EZOTools.ADDON_VERSION = "1.0.21"
 EZOTools.AUTHOR        = "@Zuriplayer"
