c46c7d9a7c3a118e5e64d580e7771be2381868ac
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 18:37:58 +0200

    Extract travel action helpers

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.101"
+EZOTools.ADDON_VERSION = "1.0.102"
 EZOTools.AUTHOR        = "@Zuriplayer"
