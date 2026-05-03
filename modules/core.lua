17eb8527d85a3d677e59a0a4236b8249404680e0
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 19:51:48 +0200

    Resolve side menu dialogs declaratively

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.105"
+EZOTools.ADDON_VERSION = "1.0.106"
 EZOTools.AUTHOR        = "@Zuriplayer"
