5ee151ec6b66195a1b11840ee5eaba3454f88ef0
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 18:14:16 +0200

    Extract friend house profiles module

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.99"
+EZOTools.ADDON_VERSION = "1.0.100"
 EZOTools.AUTHOR        = "@Zuriplayer"
