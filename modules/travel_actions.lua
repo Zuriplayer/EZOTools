cb104bbf33f002c03b994c805cf0ed70d05eb69f
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 18:37:58 +0200

    Extract travel action helpers

---
+++ modules/travel_actions.lua
@@ -0,0 +1,110 @@
+-- Acciones de viaje, grupo e instancia.
+-- Mantiene los mismos nombres publicos usados por Bindings.xml y action_exec.
+EZOTools = EZOTools or {}
+
+local EZO = EZOTools
+
+local function Print(message)
+    if EZO and type(EZO.Print) == "function" then
+        EZO.Print(message)
+    elseif type(d) == "function" then
+        d(tostring(message))
+    end
+end
+
+function EZO.JumpPrimaryHouse()
+    local id = GetHousingPrimaryHouse()
+    if id and id > 0 then
+        RequestJumpToHouse(id)
+    else
+        Print(GetString(EZO_MSG_NO_PRIMARY_HOUSE))
+    end
+end
+
+local function SaltarACasa(nombreCuenta)
+    if nombreCuenta and nombreCuenta ~= "" then
+        JumpToHouse(nombreCuenta)
+        return true
+    end
+    return false
+end
+
+function EZO.JumpCraftingHall()
+    local friends = EZO.sv and EZO.sv.friends or nil
+    if not SaltarACasa(friends and friends.craftingHall) then
+        Print(GetString(EZO_MSG_NO_CRAFTING_HALL))
+    end
+end
+
+function EZO.JumpSecondaryHall()
+    local friends = EZO.sv and EZO.sv.friends or nil
+    if not SaltarACasa(friends and friends.secondaryHall) then
+        Print(GetString(EZO_MSG_NO_SECONDARY_HALL))
+    end
+end
+
+function EZO.CanJumpToLeader()
+    if not IsUnitGrouped or not IsUnitGrouped("player") then return false end
+    if GetGroupLeaderUnitTag and CanJumpToGroupMember then
+        local leaderTag = GetGroupLeaderUnitTag()
+        if leaderTag and leaderTag ~= "" then
+            return CanJumpToGroupMember(leaderTag)
+        end
+    end
+    return JumpToGroupLeader ~= nil
+end
+
+function EZO.JumpToLeader()
+    if not IsUnitGrouped or not IsUnitGrouped("player") then
+        Print(GetString(EZO_MSG_NOT_IN_GROUP))
+        return
+    end
+
+    local leaderTag = (GetGroupLeaderUnitTag and GetGroupLeaderUnitTag()) or nil
+    if leaderTag and leaderTag ~= "" and CanJumpToGroupMember and JumpToGroupMember then
+        if CanJumpToGroupMember(leaderTag) then
+            -- CanJumpToGroupMember acepta unitTag; JumpToGroupMember necesita @Cuenta.
+            local displayName = (GetUnitDisplayName and GetUnitDisplayName(leaderTag)) or ""
+            if displayName ~= "" then
+                JumpToGroupMember(displayName)
+                return
+            end
+        end
+    end
+
+    if JumpToGroupLeader then
+        JumpToGroupLeader("")
+        return
+    end
+
+    Print(GetString(EZO_MSG_CANT_JUMP_LEADER))
+end
+
+function EZO.LeaveGroup()
+    if type(GroupLeave) == "function" then
+        GroupLeave()
+        return true
+    end
+    return false
+end
+
+function EZO.LeaveInstance()
+    if type(ExitInstanceImmediately) == "function" then
+        ExitInstanceImmediately()
+        return true
+    end
+    return false
+end
+
+function EZO.LeaveGroupAndInstance()
+    local done = false
+    if type(GroupLeave) == "function" then
+        GroupLeave()
+        done = true
+    end
+    if type(ExitInstanceImmediately) == "function" then
+        ExitInstanceImmediately()
+        done = true
+    end
+    return done
+end
