06b104b6e2897b23620dd434094e4d92e88bf001
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 10:24:40 +0200

    Extract debug diagnostics module

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.94"
+EZOTools.ADDON_VERSION = "1.0.95"
 EZOTools.AUTHOR        = "@Zuriplayer"
