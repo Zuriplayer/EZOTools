5334b2ceea26a11c0b35fc81e32b36c20dc91a3a
Zuriplayer <Zuriplayer@gmail.com>
2026-05-03 13:07:42 +0200

    Document accepted binding reuse defaults

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.96"
+EZOTools.ADDON_VERSION = "1.0.97"
 EZOTools.AUTHOR        = "@Zuriplayer"
