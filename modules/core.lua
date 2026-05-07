fdba9702a41160bd665413dce7b652db3df53c1d
Zuriplayer <Zuriplayer@gmail.com>
2026-05-07 21:44:52 +0200

    Store food quick history per character

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.114"
+EZOTools.ADDON_VERSION = "1.0.115"
 EZOTools.AUTHOR        = "@Zuriplayer"
