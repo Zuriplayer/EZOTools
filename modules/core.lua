31845acb546701f819b582141cca4171a8a4af04
Zuriplayer <Zuriplayer@gmail.com>
2026-05-09 10:15:46 +0200

    Persist food data and open housing category root

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.118"
+EZOTools.ADDON_VERSION = "1.0.120"
 EZOTools.AUTHOR        = "@Zuriplayer"
