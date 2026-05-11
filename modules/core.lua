e9cc3fa6202843c9cb93cee95739bffac33b2660
Zuriplayer <Zuriplayer@gmail.com>
2026-05-11 18:45:28 +0200

    Fix house collection quick utility

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.126"
+EZOTools.ADDON_VERSION = "1.0.127"
 EZOTools.AUTHOR        = "@Zuriplayer"
