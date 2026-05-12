a3e3c90c0b51a5ef58aa3a3b6f7e4932fab05c0c
Zuriplayer <Zuriplayer@gmail.com>
2026-05-12 14:54:51 +0200

    Fix house collection root for beta

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.0.130"
+EZOTools.ADDON_VERSION = "1.0.131"
 EZOTools.AUTHOR        = "@Zuriplayer"
