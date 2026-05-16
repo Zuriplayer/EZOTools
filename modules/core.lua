d23fb45b69220dfc5d210fbe9874f7430f43118f
Zuriplayer <Zuriplayer@gmail.com>
2026-05-16 15:58:09 +0200

    Extract side menu navigation module

--- modules/core.lua
+++ modules/core.lua
@@ -3,5 +3,5 @@
 
 -- Constantes accesibles desde cualquier módulo
 EZOTools.ADDON_NAME    = "EZOTools"
-EZOTools.ADDON_VERSION = "1.2.14"
+EZOTools.ADDON_VERSION = "1.2.15"
 EZOTools.AUTHOR        = "@Zuriplayer"
